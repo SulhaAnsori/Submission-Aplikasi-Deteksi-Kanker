a8bd0897025bc40784ebee86db2dc280d49543bc
SulhaAnsori <asulhaansori@students.amikom.ac.id>
2024-06-12 19:44:34 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.dicoding.asclepius.view
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import com.dicoding.asclepius.R
+import com.dicoding.asclepius.databinding.ActivityMainBinding
+import com.dicoding.asclepius.helper.ImageClassifierHelper
+import org.tensorflow.lite.task.vision.classifier.Classifications
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            galleryButton.setOnClickListener{startGallery()}
+            analyzeButton.setOnClickListener{analyzeImage()}
+        }
+
+    }
+
+    private fun startGallery() {
+        // TODO: Mendapatkan gambar dari Gallery.
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun showImage() {
+        // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+    // TODO: Menganalisa gambar yang berhasil ditampilkan.
+    private fun analyzeImage() {
+        currentImageUri?.let { imageUri ->
+            ImageClassifierHelper(
+                context = this,
+                classifierListener = object : ImageClassifierHelper.ClassifierListener {
+                    override fun onError(error: String) {
+                        showToast(error)
+                    }
+
+                    override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                        results?.get(0)?.let { classification ->
+                            val message = "Prediction: ${classification.label}, Score: ${classification.score}%"
+                            showToast(message)
+                            moveToResult()
+                        }
+                    }
+                }
+            ).classifyStaticImage(imageUri)
+        } ?: showToast("No image selected")
+    }
+
+
+    private fun moveToResult() {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+}
